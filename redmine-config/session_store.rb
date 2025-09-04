85b9a1110ac938932d0b4fa9f76036025c6d32e7
Matias TecnoSoul <matias@tecnosoul.com.ar>
2025-09-04 15:41:41 -0300

    1

--- redmine-config/session_store.rb
+++ redmine-config/session_store.rb
@@ -1,6 +1,6 @@
 # Configuración básica de sesiones (cookie-based)
 Rails.application.config.session_store :cookie_store,
-  key: '_redmine_session',
+  key: "_redmine_session",
   expire_after: 120.minutes,
   secure: false,
   httponly: true,
